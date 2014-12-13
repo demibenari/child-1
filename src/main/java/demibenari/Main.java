216eef6e03eaa4bca8bea0847cf22bcfba001ce1
Demi Ben-Ari <demi.benari@gmail.com>
2014-12-13 23:43:28 +0200

    Adding core classes and the usage of guice

--- src/main/java/demibenari/Main.java
+++ src/main/java/demibenari/Main.java
@@ -1,7 +1,40 @@
 package demibenari;
 
+import com.google.inject.Injector;
+import demibenari.common.authentication.SecutiryService;
+import demibenari.common.configuration.ConfigProvider;
+import demibenari.common.guice.modules.CommonModule;
+import demibenari.controllers.guice.modules.ControllersModule;
+import demibenari.common.guice.modules.factories.InjectorFactory;
+import demibenari.model.guice.modules.ModelModule;
+import demibenari.view.guice.modules.ViewModule;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start application");
+        intiateModules();
+
+        startApplication();
+    }
+
+    private static void startApplication() {
+        System.out.println("Application started");
+
+        Injector injector = InjectorFactory.getInjector();
+
+        ConfigProvider configProvider = injector.getInstance(ConfigProvider.class);
+
+        String property = configProvider.getProperty("some.property.name");
+
+        SecutiryService securityService = injector.getInstance(SecutiryService.class);
+
+
+        System.out.println("Application Finished");
+    }
+
+    private static void intiateModules() {
+        InjectorFactory.injectModule(new CommonModule());
+        InjectorFactory.injectModule(new ControllersModule());
+        InjectorFactory.injectModule(new ModelModule());
+        InjectorFactory.injectModule(new ViewModule());
     }
 }
---
+++ src/main/java/demibenari/controllers/guice/modules/ControllersModule.java
@@ -0,0 +1,13 @@
+package demibenari.controllers.guice.modules;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by Demi on 12/13/2014.
+ */
+public class ControllersModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
